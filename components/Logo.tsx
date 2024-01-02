1cb7b41272ff7a3b4c775ad920d942e3901b1f3a
Manan Khurana <khuranamanan12@gmail.com>
2024-01-02 21:52:50 +0530

    style: fixes

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -10,11 +10,9 @@ const headingFont = localFont({
 function Logo() {
   return (
     <Link href="/">
-      <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
+      <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex md:items-center">
         <Image src="/logo.svg" alt="Logo" height={30} width={30} />
-        <p
-          className={cn("text-lg text-neutral-700 pb-1", headingFont.className)}
-        >
+        <p className={cn("text-lg text-neutral-700", headingFont.className)}>
           Taskify
         </p>
       </div>
