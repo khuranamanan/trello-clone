ad3f6748b9fdb0e49e1d6bbbb332eff9c5b4fdc7
Manan Khurana <khuranamanan12@gmail.com>
2024-01-06 17:00:06 +0530

    feat: Form Components & Board Creation and Listing

--- app/(platform)/(dashboard)/_components/Navbar.tsx
+++ app/(platform)/(dashboard)/_components/Navbar.tsx
@@ -3,31 +3,38 @@ import { Button } from "@/components/ui/button";
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 import { Plus } from "lucide-react";
 import MobileSidebar from "./MobileSidebar";
+import FormPopover from "@/components/form/FormPopover";
 
 function Navbar() {
   return (
     <nav className="fixed z-50 top-0 w-full border-b shadow-sm bg-white">
       <div className="max-w-screen-2xl mx-auto flex items-center px-4 h-14">
         <MobileSidebar />
+
         <div className="flex items-center gap-x-4">
           <div className="hidden md:flex">
             <Logo />
           </div>
-          <Button
-            variant="primary"
-            size="sm"
-            className="rounded-sm hidden md:block h-auto py-1.5 px-2"
-          >
-            Create
-          </Button>
-          <Button
-            variant="primary"
-            size="sm"
-            className="rounded-sm block md:hidden"
-          >
-            <Plus className="h-4 w-4" />
-          </Button>
+          <FormPopover align="start" side="bottom" sideOffset={18}>
+            <Button
+              variant="primary"
+              size="sm"
+              className="rounded-sm hidden md:block h-auto py-1.5 px-2"
+            >
+              Create
+            </Button>
+          </FormPopover>
+          <FormPopover>
+            <Button
+              variant="primary"
+              size="sm"
+              className="rounded-sm block md:hidden"
+            >
+              <Plus className="h-4 w-4" />
+            </Button>
+          </FormPopover>
         </div>
+
         <div className="ml-auto flex items-center gap-x-2">
           <OrganizationSwitcher
             hidePersonal
--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,20 @@
+import { Separator } from "@/components/ui/separator";
+import Info from "./_components/Info";
+import BoardList from "./_components/BoardList";
+import { Suspense } from "react";
+
 function OrganizationIdPage() {
-  return <div>OrganizationPage</div>;
+  return (
+    <div className="w-full mb-20">
+      <Info />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <Suspense fallback={<BoardList.Skeleton />}>
+          <BoardList />
+        </Suspense>
+      </div>
+    </div>
+  );
 }
 
 export default OrganizationIdPage;
