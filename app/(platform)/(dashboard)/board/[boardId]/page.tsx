52fa51eccc9903c1362399673e63376d25c0b520
Manan Khurana <khuranamanan12@gmail.com>
2024-01-07 22:10:38 +0530

    feat: Add Board List

--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,5 +1,44 @@
-function BoardIdPage() {
-  return <div>BoardIdPage</div>;
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import ListContainer from "./_components/ListContainer";
+
+interface BoardIdPageProps {
+  params: {
+    boardId: string;
+  };
+}
+
+async function BoardIdPage({ params }: BoardIdPageProps) {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    redirect("/select-org");
+  }
+
+  const lists = await db.list.findMany({
+    where: {
+      boardId: params.boardId,
+      board: {
+        orgId,
+      },
+    },
+    include: {
+      cards: {
+        orderBy: {
+          order: "asc",
+        },
+      },
+    },
+    orderBy: {
+      order: "asc",
+    },
+  });
+  return (
+    <div className="p-4 h-full overflow-x-auto">
+      <ListContainer boardId={params.boardId} data={lists} />
+    </div>
+  );
 }
 
 export default BoardIdPage;
