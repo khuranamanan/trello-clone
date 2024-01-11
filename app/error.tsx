bc376e1cdb31a7cb1dd9cc3eb9d825d131c3abfd
Manan Khurana <khuranamanan12@gmail.com>
2024-01-12 01:14:44 +0530

    error page

---
+++ app/error.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+function Error() {
+  return (
+    <div className="bg-neutral-400 rounded-lg w-full h-full flex items-center justify-center">
+      <div className="text-neutral-900">Something went wrong.</div>
+    </div>
+  );
+}
+
+export default Error;
